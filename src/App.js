39e38b48ef369a45632de6b99c47402003ee48a8
YiChin Weng <yichinweng@gmail.com>
2018-10-02 00:06:56 +0800

    01 - Overview, const, let, template literals.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
+// ES6: import, export, class, extends
+// 這份檔案只是 component - App 的聲明，在 index.js 被實體化(instance)
+
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+    // const 是常數不可改變，若為 Array 或 Object 可以更新內容(pass by reference)
+    // 不變用 const，會變用 let，少用或不用 var
+    const helloWorld = 'Welcome to The Road To Learn React',
+        person = {
+          'lastName': 'Weng'
+        };
+        person.firstName = 'Winter';
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">{helloWorld}</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit <code>src/App.js</code> and save to reload. <br/>
+          I'm {`${person.firstName} ${person.lastName}`} :)
         </p>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+// 使用 App 替換 root 節點
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
