835055d29e2f1e0cbee3b4847f3a3c74c6ec2049
YiChin Weng <yichinweng@gmail.com>
2018-10-02 01:10:27 +0800

    01 - map & ES6 arrow function.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,27 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    numComments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://github.com/reduxjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    numComments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
 class App extends Component {
+  // render 是 Component Class 的方法
   render() {
     // const 是常數不可改變，若為 Array 或 Object 可以更新內容(pass by reference)
     // 不變用 const，會變用 let，少用或不用 var
@@ -24,6 +44,20 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload. <br/>
           I'm {`${person.firstName} ${person.lastName}`} :)
         </p>
+        <section className="App-intro">
+          {
+            // map: 將原陣列每一元素經由 callback 函式運算後，回傳結果，回傳值為新的陣列
+            // ES6 arrow function: 1 個參數時括號可省，回傳值時可省略 return 與花括號 (concise body)
+            list.map(item => 
+              <li key={item.objectID}>
+                <a  href={item.url}>{item.title}</a>
+                <span> By {item.author}, </span>
+                <span>{item.numComments} comments, </span>
+                <span>{item.points} points.</span>
+              </li>
+           )
+          }
+        </section>
       </div>
     );
   }
