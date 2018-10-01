b38e97fc5997a46b302881a90e45e44fa1549dcc
YiChin Weng <yichinweng@gmail.com>
2018-10-02 00:07:10 +0800

    01 - HMR.

--- src/index.js
+++ src/index.js
@@ -7,3 +7,8 @@ import registerServiceWorker from './registerServiceWorker';
 // 使用 App 替換 root 節點
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+// 開啟 Hot Module Replacement (HMR)
+if (module.hot) {
+    module.hot.accept();
+}
